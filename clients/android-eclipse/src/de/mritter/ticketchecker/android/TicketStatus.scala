3f6ae8bea233cca9127691f9db04500e064a6c9b
Marvin Ritter <marvin.ritter@gmail.com>
2013-09-27 11:52:35 +0200

    removed eclipse android client version, was not working anyway

--- clients/android-eclipse/src/de/mritter/ticketchecker/android/TicketStatus.scala
+++
@@ -1,7 +0,0 @@
-package de.mritter.ticketchecker.console
-
-class TicketStatus(val value: Int)
-case object TSUnkown extends TicketStatus(0)
-case object TKInvalid extends TicketStatus(1)
-case object CheckInSuccess extends TicketStatus(2)
-case object CheckInFailed extends TicketStatus(3)
